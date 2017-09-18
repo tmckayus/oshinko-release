697abb1c9f77b4ac2f1d0fe86d788d874afc526e
Trevor McKay <tmckay@redhat.com>
2017-09-18 10:02:29 -0400

    Add scripts for tagging existing radanalyticsio images
    The set_tag.sh and push_tag.sh scripts can be used to tag
    existing images on docker.io. For example, to mark the
    latest set of released images with a "stable" tag.

---
+++ set_tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: set_tag.sh VERSION TAG"
+    exit -1
+fi
+
+#set -e
+function pull_and_tag() {
+    docker pull docker.io/radanalyticsio/radanalytics-pyspark:$1 || return 1
+    docker pull docker.io/radanalyticsio/radanalytics-java-spark:$1 || return 1
+    docker pull docker.io/radanalyticsio/radanalytics-scala-spark:$1 || return 1
+    docker pull docker.io/radanalyticsio/oshinko-rest:$1 || return 1
+    docker pull docker.io/radanalyticsio/oshinko-webui:$1 || return 1
+
+    docker tag docker.io/radanalyticsio/radanalytics-pyspark:$1 docker.io/radanalyticsio/radanalytics-pyspark:$2 || return 1
+    docker tag docker.io/radanalyticsio/radanalytics-java-spark:$1 docker.io/radanalyticsio/radanalytics-java-spark:$2 || return 1
+    docker tag docker.io/radanalyticsio/radanalytics-scala-spark:$1 docker.io/radanalyticsio/radanalytics-scala-spark:$2 || return 1
+    docker tag docker.io/radanalyticsio/oshinko-rest:$1 docker.io/radanalyticsio/oshinko-rest:$2 || return 1
+    docker tag docker.io/radanalyticsio/oshinko-webui:$1 docker.io/radanalyticsio/oshinko-webui:$2 || return 1
+}
+
+pull_and_tag $1 $2
+if [ "$?" -eq 0 ]; then
+    echo
+    echo "Images pulled and tagged successfully"
+else
+    echo
+    echo "Failed to pull and tag all images"
+    exit -1
+fi
